7560c65aa09f8e1082649e5aab14baadf009e0e5
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2024-03-22 01:26:34 +0700

    refactor: adjust DOM for CRUD

--- script.js
+++ script.js
@@ -73,25 +73,24 @@ function addToCart(e) {
         <div class="flex gap-4">
           <img src="${newCart.image}" alt="${newCart.name}" width="50">
           <div>
-            <h1>${newCart.name}</h1>
-            <p id="price-cart-${newCart.id}" class="font-bold">${newCart.price}</p>
+            <h1>${newCart.name} <span id="qty-cart-${newCart.id}">(${newCart.quantity})</span></h1>
+            
+            <p id="price-cart-${newCart.id}" class="font-bold">${new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+    }).format(newCart.price)}</p>
           </div>
         </div>
         <div class="flex gap-2 h-fit">
-          <p id="qty-cart-${newCart.id}">${newCart.quantity}</p>
-          <button id="delete-cart-${newCart.id}" class="delete-menu p-2 text-white bg-red-700 rounded-lg">Delete</button>
+          <button id="delete-cart-${
+            newCart.id
+          }" class="delete-cart p-2 text-white bg-red-700 rounded-lg">Delete</button>
         </div>
       </div>
     `;
 
     document.getElementById('carts').innerHTML += content;
   } else {
-    const newMenus = menus.map((menu) => {
-      if (menu.id === +id) {
-        return { ...menu, stock: menu.stock - 1 };
-      }
-      return menu;
-    });
     const newCarts = carts.map((cart) => {
       if (cart.id === +id) {
         return { ...cart, quantity: cart.quantity + 1, price: cart.price + menu.price };
@@ -99,18 +98,59 @@ function addToCart(e) {
       return cart;
     });
 
-    menus = newMenus;
     carts = newCarts;
 
     const newCart = carts.find((cart) => cart.id === +id);
-    document.getElementById(`qty-cart-${newCart.id}`).innerText = newCart.quantity;
-    document.getElementById(`price-cart-${newCart.id}`).innerText = newCart.price;
+    document.getElementById(`qty-cart-${newCart.id}`).innerText = `(${newCart.quantity})`;
+    document.getElementById(`price-cart-${newCart.id}`).innerText = new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+    }).format(newCart.price);
   }
 
-  document.getElementById(`delete-cart-${menu.id}`).onclick = deleteCart;
+  const newMenus = menus.map((menu) => {
+    if (menu.id === +id) {
+      return { ...menu, stock: menu.stock - 1 };
+    }
+    return menu;
+  });
+
+  menus = newMenus;
+
+  const newMenu = menus.find((menu) => menu.id === +id);
+  document.getElementById(`stock-menu-${newMenu.id}`).innerText = `Stock: ${newMenu.stock}`;
+  document
+    .querySelectorAll('button.delete-cart')
+    .forEach((button) => (button.onclick = deleteCart));
+
+  if (!newMenu.stock) {
+    document.getElementById(newMenu.id).disabled = true;
+    document.getElementById(newMenu.id).style.backgroundColor = 'gray';
+  }
 }
 
 function deleteCart(e) {
+  const { id } = e.target;
+
+  const newMenus = menus.map((menu) => {
+    if (menu.id === +id[id.length - 1]) {
+      const restoreQty = +document.getElementById(`qty-cart-${menu.id}`).innerText[1];
+      return {
+        ...menu,
+        stock: menu.stock + restoreQty,
+      };
+    }
+    return menu;
+  });
+  const newCarts = carts.filter((cart) => cart.id !== +id[id.length - 1]);
+
+  menus = newMenus;
+  carts = newCarts;
+
+  const newMenu = menus.find((menu) => menu.id === +id[id.length - 1]);
+
+  document.getElementById(`stock-menu-${+id[id.length - 1]}`).innerText = `Stock: ${newMenu.stock}`;
+
   e.target.parentElement.parentElement.remove();
 }
 
@@ -123,8 +163,14 @@ function render() {
         <img src="${menu.image}" alt=${menu.name}>
         <div class="flex flex-col gap-2">
           <h3>${menu.name}</h3>
-          <p class="font-bold">${menu.price}</p>
-          <button id="${menu.id}" class="add-cart p-2 text-white bg-green-700 w-full rounded-lg">
+          <p class="font-bold">${new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+          }).format(menu.price)}</p>
+          <p id="stock-menu-${menu.id}">Stock: ${menu.stock}</p>
+          <button id="${
+            menu.id
+          }" class="add-cart p-2 text-white bg-green-700 hover:bg-green-800 w-full rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-auto">
               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
             </svg>
