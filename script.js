e1c5be2fcf1a1317dbd664b599c61e6d3199ac5b
son <44238478+simsonhutagaol@users.noreply.github.com>
2024-03-21 20:12:53 +0700

    feat: add menus, carts, and orders storages (#1)

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log('Hello World!');
+
+const menus = [
+    {
+        id: 1,
+        name: 'Siku Udin',
+        price: 60_000,
+        type: 'food',
+        image: 'https://marugameudon.co.id/webroot/files/MenuDetails/picture/Niku-Udon-1.jpg',
+        stock: 10,
+    },
+    {
+        id: 2,
+        name: 'Kake Udin',
+        price: 55_000,
+        type: 'food',
+        image: 'https://marugameudon.co.id/webroot/files/MenuDetails/picture/Kake-Udon-1.jpg',
+        stock: 8,
+    },
+    {
+        id: 3,
+        name: 'Tori Bata Udin',
+        price: 70_000,
+        type: 'food',
+        image: 'https://marugameudon.co.id/webroot/files/MenuDetails/picture/Chiken-Katsu-curry-udon-1.jpg',
+        stock: 15,
+    },
+    {
+        id: 4,
+        name: 'Icha',
+        price: 10_000,
+        type: 'beverage',
+        image: 'https://marugameudon.co.id/webroot/files/MenuDetails/picture/ocha.jpg',
+        stock: 30,
+    },
+    {
+        id: 5,
+        name: 'Watermolen Juice',
+        price: 20_000,
+        type: 'beverage',
+        image: 'https://marugameudon.co.id/webroot/files/MenuDetails/picture/Image%20Web_Double%20Wall_Fresh%20Watermelon%20Juice.jpg',
+        stock: 25,
+    },
+    {
+        id: 6,
+        name: 'Long Black Coffee',
+        price: 15_000,
+        type: 'beverage',
+        image: 'https://marugameudon.co.id/webroot/files/MenuDetails/picture/Image%20Web_Double%20Wall_Fresh%20Watermelon%20Juice.jpg',
+        stock: 40,
+    }
+];
+
+const carts = []
+const orders = []
