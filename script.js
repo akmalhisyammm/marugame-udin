bde6ebcdaa0801f546376ec58eafae063d34b5d5
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2024-03-22 04:56:36 +0700

    refactor: adjust DOM and HTML

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ let menus = [
 let carts = [];
 
 function addToCart(e) {
-  const { id } = e.target;
+  const { id } = e.currentTarget;
   const menu = menus.find((menu) => menu.id === +id);
   const cart = carts.find((cart) => cart.id === +id);
 
@@ -74,7 +74,6 @@ function addToCart(e) {
           <img src="${newCart.image}" alt="${newCart.name}" width="50">
           <div>
             <h1>${newCart.name} <span id="qty-cart-${newCart.id}">(${newCart.quantity})</span></h1>
-            
             <p id="price-cart-${newCart.id}" class="font-bold">${new Intl.NumberFormat('id-ID', {
       style: 'currency',
       currency: 'IDR',
@@ -84,7 +83,11 @@ function addToCart(e) {
         <div class="flex gap-2 h-fit">
           <button id="delete-cart-${
             newCart.id
-          }" class="delete-cart p-2 text-white bg-red-700 rounded-lg">Delete</button>
+          }" class="delete-cart p-2 text-white bg-red-700 hover:bg-red-800 rounded-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>
+          </button>
         </div>
       </div>
     `;
@@ -130,7 +133,7 @@ function addToCart(e) {
 }
 
 function deleteCart(e) {
-  const { id } = e.target;
+  const { id } = e.currentTarget;
 
   const newMenus = menus.map((menu) => {
     if (menu.id === +id[id.length - 1]) {
@@ -151,7 +154,10 @@ function deleteCart(e) {
 
   document.getElementById(`stock-menu-${+id[id.length - 1]}`).innerText = `Stock: ${newMenu.stock}`;
 
-  e.target.parentElement.parentElement.remove();
+  e.currentTarget.parentElement.parentElement.remove();
+
+  document.getElementById(newMenu.id).disabled = false;
+  document.getElementById(newMenu.id).style.backgroundColor = '';
 }
 
 function render() {
@@ -159,7 +165,7 @@ function render() {
 
   for (const menu of menus) {
     content += `
-      <div id="menu-${menu.id}" class="shadow-lg p-4">
+      <div id="menu-${menu.id}" class="shadow-lg flex flex-col justify-between gap-2 p-4">
         <img src="${menu.image}" alt=${menu.name}>
         <div class="flex flex-col gap-2">
           <h3>${menu.name}</h3>
